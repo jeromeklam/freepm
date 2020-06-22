ca7f685b06ad38b212e5edbbb8ada93251e21126
ericme <ericme@host.docker.internal>
2020-06-22 16:22:34 +0200

    Ajout de la gestion du lien entre version et projet.

---
+++ src/FreePM/Controller/ProjectVersion.php
@@ -0,0 +1,11 @@
+<?php
+namespace FreePM\Controller;
+
+/**
+ * Controller Project
+ *
+ * @author ericmendez
+ */
+class ProjectVersion extends \FreeFW\Core\ApiController
+{
+}
---
+++ src/FreePM/resource/routes/restful/project_version.php
@@ -0,0 +1,174 @@
+<?php
+use \FreeFW\Constants as FFCST;
+use \FreeFW\Router\Route as FFCSTRT;
+
+/**
+ * Routes for ProjectVersion
+ *
+ * @author ericmendez
+ */
+$routes_project_version = [
+    'free_p_m.project_version.autocomplete' => [
+        FFCSTRT::ROUTE_COLLECTION => 'FreePM/PrjMgnt/ProjectVersion',
+        FFCSTRT::ROUTE_COMMENT    => 'Autocomplete.',
+        FFCSTRT::ROUTE_METHOD     => FFCSTRT::METHOD_GET,
+        FFCSTRT::ROUTE_MODEL      => 'FreePM::Model::ProjectVersion',
+        FFCSTRT::ROUTE_URL        => '/v1/prj_mgnt/project_version/autocomplete/:search',
+        FFCSTRT::ROUTE_CONTROLLER => 'FreePM::Controller::ProjectVersion',
+        FFCSTRT::ROUTE_FUNCTION   => 'autocomplete',
+        FFCSTRT::ROUTE_ROLE       => \FreeFW\Router\Route::ROLE_AUTOCOMPLETE,
+        FFCSTRT::ROUTE_AUTH       => FFCSTRT::AUTH_IN,
+        FFCSTRT::ROUTE_MIDDLEWARE => [],
+        FFCSTRT::ROUTE_INCLUDE    => [],
+        FFCSTRT::ROUTE_SCOPE      => [],
+        FFCSTRT::ROUTE_PARAMETERS => [
+            'search' => [
+                FFCSTRT::ROUTE_PARAMETER_ORIGIN   => FFCSTRT::ROUTE_PARAMETER_ORIGIN_PATH,
+                FFCSTRT::ROUTE_PARAMETER_TYPE     => FFCST::TYPE_STRING,
+                FFCSTRT::ROUTE_PARAMETER_REQUIRED => true,
+                FFCSTRT::ROUTE_PARAMETER_COMMENT  => 'Chaine de recherche'
+            ],
+        ],
+        FFCSTRT::ROUTE_RESULTS    => [
+            '200' => [
+                FFCSTRT::ROUTE_RESULTS_TYPE    => FFCSTRT::RESULT_LIST,
+                FFCSTRT::ROUTE_RESULTS_MODEL   => 'FreePM::Model::ProjectVersion',
+                FFCSTRT::ROUTE_RESULTS_COMMENT => 'Réponse ok',
+            ],
+        ]
+    ],
+    'free_p_m.project_version.getall' => [
+        FFCSTRT::ROUTE_COLLECTION => 'FreePM/PrjMgnt/ProjectVersion',
+        FFCSTRT::ROUTE_COMMENT    => 'Retourne une liste filtrée, triée et paginée.',
+        FFCSTRT::ROUTE_METHOD     => FFCSTRT::METHOD_GET,
+        FFCSTRT::ROUTE_MODEL      => 'FreePM::Model::ProjectVersion',
+        FFCSTRT::ROUTE_URL        => '/v1/prj_mgnt/project_version',
+        FFCSTRT::ROUTE_CONTROLLER => 'FreePM::Controller::ProjectVersion',
+        FFCSTRT::ROUTE_FUNCTION   => 'getAll',
+        FFCSTRT::ROUTE_ROLE       => \FreeFW\Router\Route::ROLE_GET_FILTERED,
+        FFCSTRT::ROUTE_AUTH       => FFCSTRT::AUTH_IN,
+        FFCSTRT::ROUTE_MIDDLEWARE => [],
+        FFCSTRT::ROUTE_INCLUDE    => [
+            FFCSTRT::ROUTE_INCLUDE_DEFAULT => ['project']
+        ],
+        FFCSTRT::ROUTE_SCOPE      => [],
+        FFCSTRT::ROUTE_RESULTS    => [
+            '200' => [
+                FFCSTRT::ROUTE_RESULTS_TYPE    => FFCSTRT::RESULT_LIST,
+                FFCSTRT::ROUTE_RESULTS_MODEL   => 'FreePM::Model::ProjectVersion',
+                FFCSTRT::ROUTE_RESULTS_COMMENT => 'Réponse ok',
+            ],
+        ]
+    ],
+    'free_p_m.project_version.getone' => [
+        FFCSTRT::ROUTE_COLLECTION => 'FreePM/PrjMgnt/ProjectVersion',
+        FFCSTRT::ROUTE_COMMENT    => 'Retourne un objet selon son identifiant',
+        FFCSTRT::ROUTE_METHOD     => FFCSTRT::METHOD_GET,
+        FFCSTRT::ROUTE_MODEL      => 'FreePM::Model::ProjectVersion',
+        FFCSTRT::ROUTE_URL        => '/v1/prj_mgnt/project_version/:prjv_id',
+        FFCSTRT::ROUTE_CONTROLLER => 'FreePM::Controller::ProjectVersion',
+        FFCSTRT::ROUTE_FUNCTION   => 'getOne',
+        FFCSTRT::ROUTE_ROLE       => \FreeFW\Router\Route::ROLE_GET_ONE,
+        FFCSTRT::ROUTE_AUTH       => FFCSTRT::AUTH_IN,
+        FFCSTRT::ROUTE_MIDDLEWARE => [],
+        FFCSTRT::ROUTE_INCLUDE    => [
+            FFCSTRT::ROUTE_INCLUDE_DEFAULT => ['project']
+        ],
+        FFCSTRT::ROUTE_SCOPE      => [],
+        FFCSTRT::ROUTE_PARAMETERS => [
+            'prjv_id' => [
+                FFCSTRT::ROUTE_PARAMETER_ORIGIN   => FFCSTRT::ROUTE_PARAMETER_ORIGIN_PATH,
+                FFCSTRT::ROUTE_PARAMETER_TYPE     => FFCST::TYPE_BIGINT,
+                FFCSTRT::ROUTE_PARAMETER_REQUIRED => true,
+                FFCSTRT::ROUTE_PARAMETER_COMMENT  => 'Identifiant de l\'objet'
+            ],
+        ],
+        FFCSTRT::ROUTE_RESULTS    => [
+            '200' => [
+                FFCSTRT::ROUTE_RESULTS_TYPE    => FFCSTRT::RESULT_OBJECT,
+                FFCSTRT::ROUTE_RESULTS_MODEL   => 'FreePM::Model::ProjectVersion',
+                FFCSTRT::ROUTE_RESULTS_COMMENT => 'Réponse ok',
+            ],
+        ]
+    ],
+    'free_p_m.project_version.createone' => [
+        FFCSTRT::ROUTE_COLLECTION => 'FreePM/PrjMgnt/ProjectVersion',
+        FFCSTRT::ROUTE_COMMENT    => 'Créé un objet',
+        FFCSTRT::ROUTE_METHOD     => FFCSTRT::METHOD_POST,
+        FFCSTRT::ROUTE_MODEL      => 'FreePM::Model::ProjectVersion',
+        FFCSTRT::ROUTE_URL        => '/v1/prj_mgnt/project_version',
+        FFCSTRT::ROUTE_CONTROLLER => 'FreePM::Controller::ProjectVersion',
+        FFCSTRT::ROUTE_FUNCTION   => 'createOne',
+        FFCSTRT::ROUTE_ROLE       => \FreeFW\Router\Route::ROLE_CREATE_ONE,
+        FFCSTRT::ROUTE_AUTH       => FFCSTRT::AUTH_IN,
+        FFCSTRT::ROUTE_MIDDLEWARE => [],
+        FFCSTRT::ROUTE_INCLUDE    => [
+            FFCSTRT::ROUTE_INCLUDE_DEFAULT => ['project']
+        ],
+        FFCSTRT::ROUTE_SCOPE      => [],
+        FFCSTRT::ROUTE_RESULTS    => [
+            '201' => [
+                FFCSTRT::ROUTE_RESULTS_TYPE    => FFCSTRT::RESULT_OBJECT,
+                FFCSTRT::ROUTE_RESULTS_MODEL   => 'FreePM::Model::ProjectVersion',
+                FFCSTRT::ROUTE_RESULTS_COMMENT => 'Objet créé',
+            ],
+        ]
+    ],
+    'free_p_m.project_version.updateone' => [
+        FFCSTRT::ROUTE_COLLECTION => 'FreePM/PrjMgnt/ProjectVersion',
+        FFCSTRT::ROUTE_COMMENT    => 'Modifie un objet',
+        FFCSTRT::ROUTE_METHOD     => FFCSTRT::METHOD_PUT,
+        FFCSTRT::ROUTE_MODEL      => 'FreePM::Model::ProjectVersion',
+        FFCSTRT::ROUTE_URL        => '/v1/prj_mgnt/project_version/:prjv_id',
+        FFCSTRT::ROUTE_CONTROLLER => 'FreePM::Controller::ProjectVersion',
+        FFCSTRT::ROUTE_FUNCTION   => 'updateOne',
+        FFCSTRT::ROUTE_ROLE       => \FreeFW\Router\Route::ROLE_UPDATE_ONE,
+        FFCSTRT::ROUTE_AUTH       => FFCSTRT::AUTH_IN,
+        FFCSTRT::ROUTE_MIDDLEWARE => [],
+        FFCSTRT::ROUTE_INCLUDE    => [
+            FFCSTRT::ROUTE_INCLUDE_DEFAULT => ['project']
+        ],
+        FFCSTRT::ROUTE_SCOPE      => [],
+        FFCSTRT::ROUTE_PARAMETERS => [
+            'prj_id' => [
+                FFCSTRT::ROUTE_PARAMETER_ORIGIN   => FFCSTRT::ROUTE_PARAMETER_ORIGIN_PATH,
+                FFCSTRT::ROUTE_PARAMETER_TYPE     => FFCST::TYPE_BIGINT,
+                FFCSTRT::ROUTE_PARAMETER_REQUIRED => true,
+                FFCSTRT::ROUTE_PARAMETER_COMMENT  => 'Identifiant de l\'objet'
+            ],
+        ],
+        FFCSTRT::ROUTE_RESULTS    => [
+            '200' => [
+                FFCSTRT::ROUTE_RESULTS_TYPE    => FFCSTRT::RESULT_OBJECT,
+                FFCSTRT::ROUTE_RESULTS_MODEL   => 'FreePM::Model::ProjectVersion',
+                FFCSTRT::ROUTE_RESULTS_COMMENT => 'Objet modifié',
+            ],
+        ]
+    ],
+    'free_p_m.project_version.removeone' => [
+        FFCSTRT::ROUTE_COLLECTION => 'FreePM/PrjMgnt/ProjectVersion',
+        FFCSTRT::ROUTE_COMMENT    => 'Supprime un objet',
+        FFCSTRT::ROUTE_METHOD     => FFCSTRT::METHOD_DELETE,
+        FFCSTRT::ROUTE_MODEL      => 'FreePM::Model::ProjectVersion',
+        FFCSTRT::ROUTE_URL        => '/v1/prj_mgnt/project_version/:prjv_id',
+        FFCSTRT::ROUTE_CONTROLLER => 'FreePM::Controller::ProjectVersion',
+        FFCSTRT::ROUTE_FUNCTION   => 'removeOne',
+        FFCSTRT::ROUTE_AUTH       => FFCSTRT::AUTH_IN,
+        FFCSTRT::ROUTE_MIDDLEWARE => [],
+        FFCSTRT::ROUTE_INCLUDE    => [],
+        FFCSTRT::ROUTE_SCOPE      => [],
+        FFCSTRT::ROUTE_PARAMETERS => [
+            'prjv_id' => [
+                FFCSTRT::ROUTE_PARAMETER_ORIGIN   => FFCSTRT::ROUTE_PARAMETER_ORIGIN_PATH,
+                FFCSTRT::ROUTE_PARAMETER_TYPE     => FFCST::TYPE_BIGINT,
+                FFCSTRT::ROUTE_PARAMETER_REQUIRED => true,
+                FFCSTRT::ROUTE_PARAMETER_COMMENT  => 'Identifiant de l\'objet'
+            ],
+        ],
+        FFCSTRT::ROUTE_RESULTS    => [
+            '204' => [
+                FFCSTRT::ROUTE_RESULTS_COMMENT => 'Objet supprimé',
+            ]
+        ]
+    ],
+];
