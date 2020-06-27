acc76d0438df424337713139eab6fc7753914a26
jeromeklam <jeromeklam@free.fr>
2020-06-27 20:27:43 +0200

    cols

---
+++ src/FreePM/resource/routes/restful/desk_col.new.php
@@ -0,0 +1,167 @@
+<?php
+use \FreeFW\Constants as FFCST;
+use \FreeFW\Router\Route as FFCSTRT;
+
+/**
+ * Routes for DeskCol
+ *
+ * @author jeromeklam
+ */
+$routes_desk_col = [
+    'free_p_m.desk_col.autocomplete' => [
+        FFCSTRT::ROUTE_COLLECTION => 'FreePM/PrjMgnt/DeskCol',
+        FFCSTRT::ROUTE_COMMENT    => 'Autocomplete.',
+        FFCSTRT::ROUTE_METHOD     => FFCSTRT::METHOD_GET,
+        FFCSTRT::ROUTE_MODEL      => 'FreePM::Model::DeskCol',
+        FFCSTRT::ROUTE_URL        => '/v1/prj_mgnt/desk_col/autocomplete/:search',
+        FFCSTRT::ROUTE_CONTROLLER => 'FreePM::Controller::DeskCol',
+        FFCSTRT::ROUTE_FUNCTION   => 'autocomplete',
+        FFCSTRT::ROUTE_ROLE       => \FreeFW\Router\Route::ROLE_AUTOCOMPLETE,
+        FFCSTRT::ROUTE_AUTH       => FFCSTRT::AUTH_IN,
+        FFCSTRT::ROUTE_MIDDLEWARE => [],
+        FFCSTRT::ROUTE_INCLUDE    => [],
+        FFCSTRT::ROUTE_SCOPE      => [],
+        FFCSTRT::ROUTE_PARAMETERS => [
+            'search' => [
+                FFCSTRT::ROUTE_PARAMETER_ORIGIN   => FFCSTRT::ROUTE_PARAMETER_ORIGIN_PATH,
+                FFCSTRT::ROUTE_PARAMETER_TYPE     => FFCST::TYPE_STRING,
+                FFCSTRT::ROUTE_PARAMETER_REQUIRED => true,
+                FFCSTRT::ROUTE_PARAMETER_COMMENT  => 'Chaine de recherche'
+            ],
+        ],
+        FFCSTRT::ROUTE_RESULTS    => [
+            '200' => [
+                FFCSTRT::ROUTE_RESULTS_TYPE    => FFCSTRT::RESULT_LIST,
+                FFCSTRT::ROUTE_RESULTS_MODEL   => 'FreePM::Model::DeskCol',
+                FFCSTRT::ROUTE_RESULTS_COMMENT => 'Réponse ok',
+            ],
+        ]
+    ],
+    'free_p_m.desk_col.getall' => [
+        FFCSTRT::ROUTE_COLLECTION => 'FreePM/PrjMgnt/DeskCol',
+        FFCSTRT::ROUTE_COMMENT    => 'Retourne une liste filtrée, triée et paginée.',
+        FFCSTRT::ROUTE_METHOD     => FFCSTRT::METHOD_GET,
+        FFCSTRT::ROUTE_MODEL      => 'FreePM::Model::DeskCol',
+        FFCSTRT::ROUTE_URL        => '/v1/prj_mgnt/desk_col',
+        FFCSTRT::ROUTE_CONTROLLER => 'FreePM::Controller::DeskCol',
+        FFCSTRT::ROUTE_FUNCTION   => 'getAll',
+        FFCSTRT::ROUTE_ROLE       => \FreeFW\Router\Route::ROLE_GET_FILTERED,
+        FFCSTRT::ROUTE_AUTH       => FFCSTRT::AUTH_IN,
+        FFCSTRT::ROUTE_MIDDLEWARE => [],
+        FFCSTRT::ROUTE_INCLUDE    => [],
+        FFCSTRT::ROUTE_SCOPE      => [],
+        FFCSTRT::ROUTE_RESULTS    => [
+            '200' => [
+                FFCSTRT::ROUTE_RESULTS_TYPE    => FFCSTRT::RESULT_LIST,
+                FFCSTRT::ROUTE_RESULTS_MODEL   => 'FreePM::Model::DeskCol',
+                FFCSTRT::ROUTE_RESULTS_COMMENT => 'Réponse ok',
+            ],
+        ]
+    ],
+    'free_p_m.desk_col.getone' => [
+        FFCSTRT::ROUTE_COLLECTION => 'FreePM/PrjMgnt/DeskCol',
+        FFCSTRT::ROUTE_COMMENT    => 'Retourne un objet selon son identifiant',
+        FFCSTRT::ROUTE_METHOD     => FFCSTRT::METHOD_GET,
+        FFCSTRT::ROUTE_MODEL      => 'FreePM::Model::DeskCol',
+        FFCSTRT::ROUTE_URL        => '/v1/prj_mgnt/desk_col/:deco_id',
+        FFCSTRT::ROUTE_CONTROLLER => 'FreePM::Controller::DeskCol',
+        FFCSTRT::ROUTE_FUNCTION   => 'getOne',
+        FFCSTRT::ROUTE_ROLE       => \FreeFW\Router\Route::ROLE_GET_ONE,
+        FFCSTRT::ROUTE_AUTH       => FFCSTRT::AUTH_IN,
+        FFCSTRT::ROUTE_MIDDLEWARE => [],
+        FFCSTRT::ROUTE_INCLUDE    => [],
+        FFCSTRT::ROUTE_SCOPE      => [],
+        FFCSTRT::ROUTE_PARAMETERS => [
+            'deco_id' => [
+                FFCSTRT::ROUTE_PARAMETER_ORIGIN   => FFCSTRT::ROUTE_PARAMETER_ORIGIN_PATH,
+                FFCSTRT::ROUTE_PARAMETER_TYPE     => FFCST::TYPE_BIGINT,
+                FFCSTRT::ROUTE_PARAMETER_REQUIRED => true,
+                FFCSTRT::ROUTE_PARAMETER_COMMENT  => 'Identifiant de l\'objet'
+            ],
+        ],
+        FFCSTRT::ROUTE_RESULTS    => [
+            '200' => [
+                FFCSTRT::ROUTE_RESULTS_TYPE    => FFCSTRT::RESULT_OBJECT,
+                FFCSTRT::ROUTE_RESULTS_MODEL   => 'FreePM::Model::DeskCol',
+                FFCSTRT::ROUTE_RESULTS_COMMENT => 'Réponse ok',
+            ],
+        ]
+    ],
+    'free_p_m.desk_col.createone' => [
+        FFCSTRT::ROUTE_COLLECTION => 'FreePM/PrjMgnt/DeskCol',
+        FFCSTRT::ROUTE_COMMENT    => 'Créé un objet',
+        FFCSTRT::ROUTE_METHOD     => FFCSTRT::METHOD_POST,
+        FFCSTRT::ROUTE_MODEL      => 'FreePM::Model::DeskCol',
+        FFCSTRT::ROUTE_URL        => '/v1/prj_mgnt/desk_col',
+        FFCSTRT::ROUTE_CONTROLLER => 'FreePM::Controller::DeskCol',
+        FFCSTRT::ROUTE_FUNCTION   => 'createOne',
+        FFCSTRT::ROUTE_ROLE       => \FreeFW\Router\Route::ROLE_CREATE_ONE,
+        FFCSTRT::ROUTE_AUTH       => FFCSTRT::AUTH_IN,
+        FFCSTRT::ROUTE_MIDDLEWARE => [],
+        FFCSTRT::ROUTE_INCLUDE    => [],
+        FFCSTRT::ROUTE_SCOPE      => [],
+        FFCSTRT::ROUTE_RESULTS    => [
+            '201' => [
+                FFCSTRT::ROUTE_RESULTS_TYPE    => FFCSTRT::RESULT_OBJECT,
+                FFCSTRT::ROUTE_RESULTS_MODEL   => 'FreePM::Model::DeskCol',
+                FFCSTRT::ROUTE_RESULTS_COMMENT => 'Objet créé',
+            ],
+        ]
+    ],
+    'free_p_m.desk_col.updateone' => [
+        FFCSTRT::ROUTE_COLLECTION => 'FreePM/PrjMgnt/DeskCol',
+        FFCSTRT::ROUTE_COMMENT    => 'Modifie un objet',
+        FFCSTRT::ROUTE_METHOD     => FFCSTRT::METHOD_PUT,
+        FFCSTRT::ROUTE_MODEL      => 'FreePM::Model::DeskCol',
+        FFCSTRT::ROUTE_URL        => '/v1/prj_mgnt/desk_col/:deco_id',
+        FFCSTRT::ROUTE_CONTROLLER => 'FreePM::Controller::DeskCol',
+        FFCSTRT::ROUTE_FUNCTION   => 'updateOne',
+        FFCSTRT::ROUTE_ROLE       => \FreeFW\Router\Route::ROLE_UPDATE_ONE,
+        FFCSTRT::ROUTE_AUTH       => FFCSTRT::AUTH_IN,
+        FFCSTRT::ROUTE_MIDDLEWARE => [],
+        FFCSTRT::ROUTE_INCLUDE    => [],
+        FFCSTRT::ROUTE_SCOPE      => [],
+        FFCSTRT::ROUTE_PARAMETERS => [
+            'deco_id' => [
+                FFCSTRT::ROUTE_PARAMETER_ORIGIN   => FFCSTRT::ROUTE_PARAMETER_ORIGIN_PATH,
+                FFCSTRT::ROUTE_PARAMETER_TYPE     => FFCST::TYPE_BIGINT,
+                FFCSTRT::ROUTE_PARAMETER_REQUIRED => true,
+                FFCSTRT::ROUTE_PARAMETER_COMMENT  => 'Identifiant de l\'objet'
+            ],
+        ],
+        FFCSTRT::ROUTE_RESULTS    => [
+            '200' => [
+                FFCSTRT::ROUTE_RESULTS_TYPE    => FFCSTRT::RESULT_OBJECT,
+                FFCSTRT::ROUTE_RESULTS_MODEL   => 'FreePM::Model::DeskCol',
+                FFCSTRT::ROUTE_RESULTS_COMMENT => 'Objet modifié',
+            ],
+        ]
+    ],
+    'free_p_m.desk_col.removeone' => [
+        FFCSTRT::ROUTE_COLLECTION => 'FreePM/PrjMgnt/DeskCol',
+        FFCSTRT::ROUTE_COMMENT    => 'Supprime un objet',
+        FFCSTRT::ROUTE_METHOD     => FFCSTRT::METHOD_DELETE,
+        FFCSTRT::ROUTE_MODEL      => 'FreePM::Model::DeskCol',
+        FFCSTRT::ROUTE_URL        => '/v1/prj_mgnt/desk_col/:deco_id',
+        FFCSTRT::ROUTE_CONTROLLER => 'FreePM::Controller::DeskCol',
+        FFCSTRT::ROUTE_FUNCTION   => 'removeOne',
+        FFCSTRT::ROUTE_ROLE       => \FreeFW\Router\Route::ROLE_DELETE_ONE,
+        FFCSTRT::ROUTE_AUTH       => FFCSTRT::AUTH_IN,
+        FFCSTRT::ROUTE_MIDDLEWARE => [],
+        FFCSTRT::ROUTE_INCLUDE    => [],
+        FFCSTRT::ROUTE_SCOPE      => [],
+        FFCSTRT::ROUTE_PARAMETERS => [
+            'deco_id' => [
+                FFCSTRT::ROUTE_PARAMETER_ORIGIN   => FFCSTRT::ROUTE_PARAMETER_ORIGIN_PATH,
+                FFCSTRT::ROUTE_PARAMETER_TYPE     => FFCST::TYPE_BIGINT,
+                FFCSTRT::ROUTE_PARAMETER_REQUIRED => true,
+                FFCSTRT::ROUTE_PARAMETER_COMMENT  => 'Identifiant de l\'objet'
+            ],
+        ],
+        FFCSTRT::ROUTE_RESULTS    => [
+            '204' => [
+                FFCSTRT::ROUTE_RESULTS_COMMENT => 'Objet supprimé',
+            ]
+        ]
+    ],
+];
